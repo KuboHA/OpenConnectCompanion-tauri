7bbd35da060fbb47baf85f31a97a7405cad6c284
KuboHA <jakubschalek17@gmail.com>
2026-01-11 21:52:45 +0100

    fix appimage issues

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+  // Fix for WebKitGTK rendering issues on Linux (e.g. EGL_BAD_PARAMETER)
+  #[cfg(target_os = "linux")]
+  std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
   app_lib::run();
 }
